d91f0e4fa7afa2c7248a62276b4331668b773816
Frederic Mineur <fredericmineur@gmail.com>
2021-10-06 11:34:10 +0100

    add reusable wrapper Card component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Card from '../UI/Card';
+import styles from './AddUser.module.css'
 
 const AddUser = props => {
 
@@ -8,13 +10,16 @@ const AddUser = props => {
     }
 
     return (
-        <form onSubmit={addUserHandler}>
+        <Card className={styles.input}>
+            <form onSubmit={addUserHandler}>
             <label htmlFor="username">Username</label>
             <input id="username" type="text"/>
             <label htmlFor="age">Age (Years)</label>
             <input id="age" type="number"/>
             <button type="submit">Add user</button>
         </form>
+        </Card>
+        
     );
 }
 
