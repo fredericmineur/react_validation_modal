44d599e477523f34238bd4f23cf1c8896f23d3fb
Frederic Mineur <fredericmineur@gmail.com>
2021-10-11 15:52:44 +0100

    Wrapper and Fragment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import React, {useState} from 'react';
-import AddUser from './components/Users/AddUser';
-import UsersList from './components/Users/UsersList';
-
+import React, { useState, Fragment } from "react";
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
+import Wrapper from "./components/Helpers/Wrapper";
 
 function App() {
-
-const [users, setUsers] = useState([]);
-const userListHandler = (username, age) => {
-  setUsers(prevUsers => {
-    return [...prevUsers, {username, age, id: Math.random().toString()}]
-  })
-
-}
+  const [users, setUsers] = useState([]);
+  const userListHandler = (username, age) => {
+    setUsers((prevUsers) => {
+      return [...prevUsers, { username, age, id: Math.random().toString() }];
+    });
+  };
 
   return (
-    <div>
+    <Fragment>
       <AddUser onAddUser={userListHandler} />
-      <UsersList users={users}/>
-    </div>
+      <UsersList users={users} />
+    </Fragment>
   );
 }
 
